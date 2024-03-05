a52e64309cc216bf0727903065d6ddad8577939d
Manish Kolla <68372461+manishkolla@users.noreply.github.com>
2024-03-05 16:34:48 -0500

    Delete Resume_Parser directory

--- Resume_Parser/resume_parser.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials,db
-
-cred = credentials.Certificate("creds.json")
-firebase_admin.initialize_app(cred, {"databaseURL": "https://careerswipe-f6d67-default-rtdb.firebaseio.com/"})
-
-ref= db.reference("/")
-ref.get()
-

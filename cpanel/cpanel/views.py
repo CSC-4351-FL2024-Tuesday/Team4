90ba73c289afe353cd14c053838577b4b9495f5a
Nishchay Patel <nickpat31022@gmail.com>
2024-04-08 17:03:47 -0400

    Backend code running online, without local download-Author Manish

--- cpanel/cpanel/views.py
+++ cpanel/cpanel/views.py
@@ -20,16 +20,22 @@ from firebase_admin import credentials,db
 from firebase_admin import storage
 import glob
 import requests
-
 from django.shortcuts import render,redirect
 import pyrebase
-from django.contrib import auth
 from django import forms
 import os
 import json
+import urllib.request
+import tempfile
+import docx
+import sys
+from dotenv import load_dotenv
+sys.path.append(r"C:\Users\npatel237\OneDrive - Georgia State University\CareerSwipe\cpanel")
+from config import config
+from config import cred as creds
 
-API=''
-
+load_dotenv()
+API=os.getenv("API")
 
 class UploadFileForm(forms.Form):
     file = forms.FileField()
@@ -45,26 +51,98 @@ def file_ext(file_path,user_id):
     response = requests.get(url)
    
    def pdf(file_path):
+      print("came to pdf method!!!")
       try:
-         response = requests.get(file_path)
-      except:
-         pass
-      with open(file_path, 'rb') as file:
-         pdf_reader = PyPDF2.PdfReader(file)
+         response = urllib.request.urlopen(file_path)
+         pdf_content = response.read()
+      except Exception as e:
+         # Handle exception if URL couldn't be opened
+         print("Error:", e)
+         return None
+
+      with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+         temp_file.write(pdf_content)
+         temp_file.seek(0)
+
+         # Read the PDF content from the temporary file using PyPDF2
+         pdf_reader = PyPDF2.PdfReader(temp_file)
          text = ""
          page = pdf_reader.pages[0]  # Extract text from only the first page
          page_text = page.extract_text()
          text += page_text
+      response.close()
       return text
 
-   def wdocx(file_path):
-      text = textract.process(file_path)
-      return text.decode('utf-8')
-
-   def txt(file_path):
-      with open(file_path, "r", encoding='latin-1') as f:
-         text = f.read()
+   def wdocx(file_url):
+      # Download the file from the URL
+      response = requests.get(file_url)
+      
+      # Save the file to a temporary location
+      import tempfile
+      with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+         temp_file.write(response.content)
+         temp_file_path = temp_file.name
+      
+      # Extract the text from the file
+         print("1")
+         try:
+            #text = textract.process(temp_file_path)
+            doc = docx.Document(temp_file_path)
+            text = '\n'.join([paragraph.text for paragraph in doc.paragraphs])
+         except:
+            print("EXE")
+            text = ""
+            text = textract.process(temp_file_path, encoding='utf-8')
+         print("2")
+      # Clean up the temporary file
+      import os
+      os.remove(temp_file_path)
+      
+      # Return the extracted text
       return text
+   
+   def txt(url):
+      try:
+         # Open the URL and read the content
+         response = urllib.request.urlopen(url)
+         content = response.read()
+         
+         # Decode the content as a string
+         text = content.decode('utf-8')
+         
+         # Close the response
+         response.close()
+         
+         return text
+      except urllib.error.URLError as e:
+         print(f"Error: {e}")
+         return None
+   
+# ######Before Modified#######################
+#    def pdf2(file_path):
+#       try:
+#          #response = requests.get(file_path)
+#          response = urllib.request.urlopen(file_path)
+#       except:
+#          pass
+#       with open(file_path, 'rb') as file:
+#          pdf_reader = PyPDF2.PdfReader(file)
+#          text = ""
+#          page = pdf_reader.pages[0]  # Extract text from only the first page
+#          page_text = page.extract_text()
+#          text += page_text
+#       return text
+
+#    def wdocx2(file_path):
+#       text = textract.process(file_path)
+#       return text.decode('utf-8')
+   
+
+#    def txt2(file_path):
+#       with open(file_path, "r", encoding='latin-1') as f:
+#          text = f.read()
+#       return text
+# ######Before Modified#######################
 
    def resume_data(file_content, user_id):
       
@@ -73,57 +151,78 @@ def file_ext(file_path,user_id):
       chat = model.start_chat()  
       
       chat.send_message(file_content)  
-      questions=[]
       final = ''
-      with open(r'C:\Users\Nishc\OneDrive - Georgia State University\CareerSwipe\cpanel\Questions.txt', "r") as f:
-         questions = f.readlines()  
+
+
+      # quest=("https://firebasestorage.googleapis.com/v0/b/careerswipe-c08a4.appspot.com/o/Master%2FQuestions.txt?alt=media&token=1fb5936b-402b-43a1-b21d-b59fa308d13a")
+      # with open(quest) as f:
+      #    questions = f.read()  
+      # q_url=storage.child('Master').child('Questions.txt').get_url(None)
+      # print("Questions url: ", q_url)
+      # questions=txt(q_url)
+      questions=['Give the answer of the following in a single json format: "Personal Info" : Name, Email, Phone Number, and Location."Education"  : University, GPA, Major and Minor. "Skills" : Such as 8 programming skills, 5 technological skills, 5 behavioural skills. "Work Experience": Position, Company, Location, Dates, Small summary of that role. "Extracurricular Activities": Avtivity, Role. All the links present in this text. "Suggestions": Suggest what are the 5 best roles for this candidate in future. "Score" : Based on the candidates major GPA, give score out of 100. "Improvement Suggestions" : List 3 things that this candidate do to improve the resume.']
+      # storage_app = firebase_admin.get_app()
+      # storage_client = storage.client(app=storage_app)
+      # bucket = storage_client.bucket()
 
       for question in questions:
          response = chat.send_message(question)
          answer = response.text.replace('python', '').replace('```', '').replace('candidate_info = {', '').replace('json', '')
          final += answer
-
+      global final_json
       final_json = json.loads(final)
-      file_path_json = r"C:\Users\Nishc\OneDrive - Georgia State University\CareerSwipe\cpanel\text_data.json"
-      with open(file_path_json, 'w') as json_file:
-         json.dump(final_json, json_file)
+      # global file_path_json
+      # final_json_str = json.dumps(final_json)
+      # print(final_json_str)
+      # storage.child('jsonfiles').child(f'{user_id}.json').put(final_json_str)
 
-      with open(file_path_json) as f:
-         data = json.load(f)
-      print(type(data))
-      return data
+      #bucket = storage.bucket()
+      # blob = bucket.blob(f'jsonfiles/{user_id}.json')
+      # blob.upload_from_string(final_json_str, content_type='application/json')
+      # print('json uploaded')
+      # global file_path_json
+      # file_path_json = r"C:\Users\mkolla1\Downloads\carreerr\cpanel\text_data.json"
+      # with open(file_path_json, 'w') as json_file:
+      #    json.dump(final_json, json_file)
+
+      # with open(file_path_json) as f:
+      #    data = json.load(f)
+      # print(type(data))
+      # print("DATA IS", data)
+      # print("types are",type(data),type(final_json) )
+      return final_json
 
    def file_read(file_path):
-      if file_path.endswith('.txt'):
+      if file_path.endswith('.txt') or '.txt' in file_path:
          return txt(file_path)
-      elif file_path.endswith('.docx'):
+      elif file_path.endswith('.docx') or '.docx' in file_path:
+         print('its a word document')
          return wdocx(file_path)
-      elif file_path.endswith('.pdf'):
+      elif file_path.endswith('.pdf') or '.pdf' in file_path:
          return pdf(file_path)
       else:
          return 'Upload only PDF, DOCX, TXT. Try Again'
 
    print('Made it')
    file_content=file_read(file_path)
+   print("PDF file reading done")
    data=resume_data(file_content,user_id)
+   print("getting text done")
    return data
 
 
 #######################################################################################################################################
 
-from django.shortcuts import render,redirect
-import pyrebase
-from django.contrib import auth
-from django import forms
-import os
-#'storageBucket': "careerswipe-c08a4.appspot.com",
-config={}
+
+
 firebase=pyrebase.initialize_app(config)
 autho=firebase.auth()
 database=firebase.database()
 storage=firebase.storage()
+
+
 try:
-   cred = credentials.Certificate(r"C:\Users\Nishc\OneDrive - Georgia State University\CareerSwipe\cpanel\creds.json")
+   cred = credentials.Certificate(creds)
    firebase_admin.initialize_app(cred, {"databaseURL": config['databaseURL']})
 except:
    print("Wrong CREDENTIALS!!!!!!!!!!!!!")
@@ -135,7 +234,7 @@ def signIn(request):
 def postsignin(request):
       global user
       email=request.POST.get('email')
-      password=request.POST.get('Password')
+      password=request.POST.get('password')
       try:
          user=autho.sign_in_with_email_and_password(email,password)
          #print(user['idToken']) 
@@ -159,7 +258,7 @@ def signup(request):
 def postsignup(request):
    name=request.POST.get('name')
    email=request.POST.get('email')
-   password=request.POST.get('Password')
+   password=request.POST.get('password')
    confirmpassword=request.POST.get('confirmpassword')
    if confirmpassword!=password:
       message="Password didnt match"
@@ -189,37 +288,44 @@ def uploadfile(request):
       my_resume=request.FILES['file']
       my_resume_name=my_resume.name
       my_resume_name=user_id+my_resume_name
+      print("resume type is : ",type(my_resume))
       storage.child(my_resume_name).put(my_resume)
       try:
-         storage.child(my_resume_name).download(filename='resume'+my_resume.name,path='/cpanel')
+         #storage.child(my_resume_name).download(filename='resume'+my_resume.name,path='/cpanel')
+         download_url = storage.child(my_resume_name).get_url(None)
+         print(download_url)
       except:
           return render(request,'welcome.html',{"message":'Retry upload'})
       url='gs://careerswipe-c08a4.appspot.com/'+my_resume_name
       print(url)
-      garbage='?alt=media&token=49865b6d-c782-4889-b5be-5c97bb472f32'
-      web_path=(r'https://firebasestorage.googleapis.com/v0/b/careerswipe-c08a4.appspot.com/o/'+my_resume_name+garbage)
+      web_path=(r'https://firebasestorage.googleapis.com/v0/b/careerswipe-c08a4.appspot.com/o/'+my_resume_name)
       #web_path=(r'gs://careerswipe-c08a4.appspot.com/'+my_resume_name)
-      local_path=(r'C:\Users\Nishc\OneDrive - Georgia State University\CareerSwipe\cpanel\resume'+my_resume.name)
-      print((local_path))
+
+      
+      #local_path=(r'C:\Users\mkolla1\Downloads\carreerr\cpanel\resume'+my_resume.name)
+      #print((local_path))
       print(user_id)
-      data_file=file_ext(local_path,user_id)
+      data_file=file_ext(download_url,user_id)
       print(type(data_file))
       ref = db.reference(f'/student/{user_id}')
-      try:
-         with open(r"C:\Users\Nishc\OneDrive - Georgia State University\CareerSwipe\cpanel\text_data.json", 'r') as json_file:
-            resume_date = json.load(json_file)
-            print(type(resume_date))
-            ref.set(resume_date)
-      except FileNotFoundError:
-         print("Error: File not found. Please check the file path.")
+      ref.set(final_json)
+      # try:
+      #    #with open(r"C:\Users\mkolla1\Downloads\carreerr\cpanel\text_data.json", 'r') as json_file:
+      #    with open(fr'{file_path_json}', 'r') as json_file:
+      #       resume_date = json.load(json_file)
+      #       print(type(resume_date))
+      #       ref.set(final_json)
+      # except FileNotFoundError:
+      #    print("Error: File not found. Please check the file path.")
       
       # data={'Resume':url}
       # database.set(data)
       # database.child('users').child(user_id).child('details').update(data_file)
-      return render(request,'uploadsuccess.html',{'file':resume_date})
+      return render(request,'uploadsuccess.html',{'file':final_json})
    else:
       form=UploadFileForm()
-      message= resume_date
+      #message= resume_date
+      message='form not valid'
       return render(request,'welcome.html',{'form':form, 'message':message})
 
 def forgotpassword(request):

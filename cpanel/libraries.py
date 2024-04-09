a5e7238a83de86cbc76fd1774d908e9385312699
Nishchay Patel <156350451+npatel237@users.noreply.github.com>
2024-04-09 14:16:23 -0400

    Add files via upload
    Libraries needed for backend so far

---
+++ cpanel/libraries.py
@@ -0,0 +1,33 @@
+#3/27/2024
+import pathlib
+import textwrap
+import google.generativeai as genai
+import PyPDF2
+import docx
+from IPython.display import display
+from IPython.display import Markdown
+import google.generativeai as genai
+import json
+import os
+import textract
+from django.shortcuts import render,redirect
+import pyrebase
+from django.contrib import auth
+from django import forms
+from django.http import HttpResponseRedirect
+import firebase_admin
+from firebase_admin import credentials,db
+from firebase_admin import storage
+import glob
+import requests
+from django.shortcuts import render,redirect
+import pyrebase
+from django import forms
+import os
+import json
+import urllib.request
+import tempfile
+import docx
+import sys
+import tempfile
+from dotenv import load_dotenv
